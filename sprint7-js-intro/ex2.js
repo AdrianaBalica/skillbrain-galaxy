b387c2c5a868ce454dbc6febcd39875824e01309
AdrianaBalica <balica.adi1986@gmail.com>
2025-09-14 22:48:58 +0300

    am terminat ex2

---
+++ sprint7-js-intro/ex2.js
@@ -0,0 +1,10 @@
+function tellFortune(x, y, z, n) {
+    return "Vei fi " + x + " in " + y + ", casatorit cu " + z + " si vei avea " + n + " copii.";
+}
+
+const x="Programator";
+const y="Italia";
+const z="Emanuel";
+const n="3";
+
+console.log(tellFortune(x, y, z, n));
