45b7b5c6703039a98103ef4362541da83d5ca459
AdrianaBalica <balica.adi1986@gmail.com>
2025-09-15 09:40:01 +0300

    am terminat ex3

---
+++ sprint7-js-intro/ex3.js
@@ -0,0 +1,51 @@
+let zi= 1;
+
+switch(zi){
+    case 1:
+        console.log("Luni");
+        break;
+    case 2:
+        console.log("Marti");
+        break;
+    case 3:
+        console.log("Miercuri");
+        break;
+    case 4:
+        console.log("Joi");
+        break; 
+    case 5:
+        console.log("Vineri");
+        break;
+    case 6:
+        console.log("Sambata");
+        break;
+    case 7:
+        console.log("Duminica");
+        break;
+}
+
+let zi= 5;
+
+switch(zi){
+    case 1:
+        console.log("Luni");
+        break;
+    case 2:
+        console.log("Marti");
+        break;
+    case 3:
+        console.log("Miercuri");
+        break;
+    case 4:
+        console.log("Joi");
+        break; 
+    case 5:
+        console.log("Vineri");
+        break;
+    case 6:
+        console.log("Sambata");
+        break;
+    case 7:
+        console.log("Duminica");
+        break;
+}
