2a7bd1c1523d31b78548567d26e04c13c21bca77
AdrianaBalica <balica.adi1986@gmail.com>
2025-09-18 21:04:42 +0300

    am terminat ex2 si ex3

--- sprint8/ex2.js
+++ sprint8/ex2.js
@@ -4,3 +4,13 @@ const suma = numbers.reduce((total, num) => total + num, 0);
 
 console.log(suma); 
 
+
+
+const numbers1 = [3.45, -2.68, 356, -75.96, 64, 19.28];
+
+function sumaNumerelor(total,num) {
+    return total + num;
+}
+const first = numbers1.reduce (sumaNumerelor, 0);
+
+console.log(first);
---
+++ sprint8/ex3.js
@@ -0,0 +1,5 @@
+const numbers = [1, -5, 20, -34, 16, 29, 36, -4];
+
+const ordine = numbers.reverse()
+
+console.log(ordine); 
