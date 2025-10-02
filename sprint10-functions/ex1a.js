8c42b5bc5948ae0b7f78c58dace0ed1f7e6e80d0
AdrianaBalica <balica.adi1986@gmail.com>
2025-10-02 22:01:50 +0300

    am adaugat o linie in ex 1a

--- sprint10-functions/ex1a.js
+++ sprint10-functions/ex1a.js
@@ -2,4 +2,5 @@ function findSquares(numbers) {
     return numbers.map(num => num * num);
   }
 
-console.log("Patratele numerelor 6, 0, si -12 sunt ", findSquares([6, 0, 12]));
+console.log("Patratele numerelor 6, 0, si -12 sunt ", findSquares([6, 0, -12]));
+console.log("Patratele numerelor 5, 7, si 25 sunt ", findSquares([5, 7, 25]));
